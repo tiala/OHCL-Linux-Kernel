bb2916917c9fefd3faae8e24451e02ce53e07773
Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>
2025-07-09 09:02:39 +0530

    x86/apic: Add kexec support for Secure AVIC
    Add a apic->teardown() callback to disable Secure AVIC before
    rebooting into the new kernel. This ensures that the new
    kernel does not access the old APIC backing page which was
    allocated by the previous kernel. Such accesses can happen
    if there are any APIC accesses done during guest boot before
    Secure AVIC driver probe is done by the new kernel (as Secure
    AVIC would have remained enabled in the Secure AVIC control
    msr).
    
    Signed-off-by: Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>

--- arch/x86/include/asm/sev.h
+++ arch/x86/include/asm/sev.h
@@ -415,6 +415,7 @@ void sev_show_status(void);
 void snp_update_svsm_ca(void);
 void snp_mshv_vtl_return(u8 target_vtl);
 enum es_result savic_register_gpa(u64 gpa);
+enum es_result savic_unregister_gpa(u64 *gpa);
 u64 savic_ghcb_msr_read(u32 reg);
 void savic_ghcb_msr_write(u32 reg, u64 value);
 
@@ -455,6 +456,7 @@ static inline void sev_show_status(void) { }
 static inline void snp_update_svsm_ca(void) { }
 static inline void snp_mshv_vtl_return(u8 input_vtl) { }
 static inline enum es_result savic_register_gpa(u64 gpa) { return ES_UNSUPPORTED; }
+static inline enum es_result savic_unregister_gpa(u64 *gpa) { return ES_UNSUPPORTED; }
 static inline void savic_ghcb_msr_write(u32 reg, u64 value) { }
 static inline u64 savic_ghcb_msr_read(u32 reg) { return 0; }
 
